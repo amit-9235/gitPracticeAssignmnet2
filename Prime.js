1e4f7c6fc3ac0912070dbd4798462d6f0e0b7a04
unknown <amit.ak4746@gmail.com>
2023-02-18 01:54:54 +0530

    added even-odd

--- Prime.js
+++ Prime.js
@@ -1,15 +1,15 @@
 count = 0;
-let N=9
+let N=9;
 for(let i=0; i<=N; i++){
 if(N%i==0){
 count++
 }
 }
 if(count==2){
-console.log("Prime)
+console.log("Prime);
 }
 else{
 
-console.log("Not Prime)
+console.log("Not Prime);
 }
 
---
+++ even_odd.js
@@ -0,0 +1,7 @@
+let n = 14;
+if(n%2==0){
+console.log("It is Even")
+}
+else{
+console.log("Not Even")
+}
